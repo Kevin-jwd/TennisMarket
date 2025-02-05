7c86ee745ecb5e3bf1c6ab301aacb7d69f76aa91
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-02-05 20:46:38 +0900

    feat: implement product order system and order list
    - create order handling and insertion into orderlist table in DB
    - update main view and order page with correct product links
    - adjust routing and URL parsing for productId handling
    - implement order list view displaying product order data
    - adjust image and CSS file handling

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ let url = require('url');
 
 function start(route, handle) {
     function onRequest(request, response) {
-        let pathname = url.parse(request.url).pathname;
-        let queryData = url.parse(request.url, true).query;
-
-        route(pathname, handle, response, queryData.productId);
+        let pathname = new URL(request.url, `http://${request.headers.host}`).pathname; 
+        let queryData = new URL(request.url, `http://${request.headers.host}`).searchParams; 
+        route(pathname, handle, response, queryData.get("productId"));
     }
 
     http.createServer(onRequest).listen(8888);
