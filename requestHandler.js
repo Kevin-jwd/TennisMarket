ad8b8d7c4e18a319a88cb9503c04c251d2820a99
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-02-05 14:27:49 +0900

    feat: integrate main.html with backend and update file paths

--- requestHandler.js
+++ requestHandler.js
@@ -1,3 +1,7 @@
+// file sync 모듈 불러오기
+const fs = require('fs');
+const main_view = fs.readFileSync('./main.html', 'utf-8');
+
 // 데이터베이스 연결 모듈 불러오기
 const mariadb = require('D:/DevCourse-6/3주차/database/connect/mariadb.js')
 
@@ -10,19 +14,11 @@ function main(response) {
     });
 
     response.writeHead(200, {'Content-Type' : 'text/html'});
-    response.write('Main page');
-    response.end();
-}
-
-function login(response) {
-    console.log('Response : login');
-    response.writeHead(200, {'Content-Type' : 'text/html'});
-    response.write('Login page');
+    response.write(main_view);
     response.end();
 }
 
 let handle = {};            // key:value
 handle['/'] = main;
-handle['/login'] = login;
 
 exports.handle = handle;
